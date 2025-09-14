7c2e132bc06e2766cbb74eac11b4b33ce4b441e2
jackypck <jackypck@bu.edu>
2025-09-14 12:18:22 -0400

    add secret and var to ci.yml

---
+++ config/set_env_var.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export LANGSMITH_ENDPOINT=https://api.smith.langchain.com
+export LANGSMITH_PROJECT="default"
+export LANGSMITH_TRACING=true
+export LANGCHAIN_CALLBACKS_BACKGROUND=false
